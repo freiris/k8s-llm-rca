28693877eb0f6721cc565c3102dfbd28ff381b82
ubuntu <xiangyong08@gmail.com>
2024-07-08 01:15:42 -0700

    run each category of error-message in full-test, modify it based on mini-test

---
+++ full-test/run-100-Evicted.sh
@@ -0,0 +1,6 @@
+# for Evicted-LowOnResource-repeat-refined-10
+python3 ../test_with_file_args_extend_index.py -i ./data-4/Evicted-LowOnResource-repeat.csv -o ./output-4/Evicted-LowOnResource-repeat-out-0703.json -b 3 -e 100 
+
+# for Evicted-NodeDiskPressure-repeat-refined-10
+python3 ../test_with_file_args_extend_index.py -i ./data-4/Evicted-NodeDiskPressure-repeat.csv -o ./output-4/Evicted-NodeDiskPressure-repeat-out-0703.json -b 3 -e 100 
+
---
+++ full-test/run-100-FailedMount.sh
@@ -0,0 +1,21 @@
+# for FailedMount-ConfigMapNotFound
+python3 ../test_with_file_args_extend_index.py -i ./data-4/FailedMount-ConfigMapNotFound.csv -o ./output-4/FailedMount-ConfigMapNotFound-out-0703.json -b 3 -e 100
+
+# for FailedMount-FailedSyncConfigMapCache
+python3 ../test_with_file_args_extend_index.py -i ./data-4/FailedMount-FailedSyncConfigMapCache.csv -o ./output-4/FailedMount-FailedSyncConfigMapCache-out-0703.json -b 3 -e 100
+
+# for FailedMount-FailedSyncSecretCache
+python3 ../test_with_file_args_extend_index.py -i ./data-4/FailedMount-FailedSyncSecretCache.csv -o ./output-4/FailedMount-FailedSyncSecretCache-out-0703.json -b 3 -e 100
+
+# for FailedMount-NoSuchFileDir
+python3 ../test_with_file_args_extend_index.py -i ./data-4/FailedMount-NoSuchFileDir.csv -o ./output-4/FailedMount-NoSuchFileDir-out-0703.json -b 3 -e 100
+
+# for FailedMount-ObjectNotRegistered
+python3 ../test_with_file_args_extend_index.py -i ./data-4/FailedMount-ObjectNotRegistered-repeat.csv -o ./output-4/FailedMount-ObjectNotRegistered-repeat-out-0703.json -b 3 -e 100
+
+# for FailedMount-SecretNotFound
+python3 ../test_with_file_args_extend_index.py -i ./data-4/FailedMount-SecretNotFound.csv -o ./output-4/FailedMount-SecretNotFound-out-0703.json -b 3 -e 100
+
+# for FailedMount-StaleNFS
+python3 ../test_with_file_args_extend_index.py -i ./data-4/FailedMount-StaleNFS-repeat.csv -o ./output-4/FailedMount-StaleNFS-repeat-out-0703.json -b 3 -e 100
+
