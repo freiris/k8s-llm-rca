fc0cdcdeb41b19b8796f5170c07524b83ebd2dca
ubuntu <xiangyong08@gmail.com>
2024-07-08 01:10:09 -0700

    Add only .sh files from mini-test directory

---
+++ mini-test/run-10-FailedCreate.sh
@@ -0,0 +1,12 @@
+# for FailedCreate-ExceedQuota-Job-refined-10
+python3 ../test_with_file_args_extend_index.py -i ./input-head-10/FailedCreate-ExceedQuota-Job-refined-10.csv -o ./out-head-10/FailedCreate-ExceedQuota-Job-refined-10-out-0628.json -b 0 -e 3
+
+# for FailedCreate-ExceedQuota-ReplicaSet-refined-10
+python3 ../test_with_file_args_extend_index.py -i ./input-head-10/FailedCreate-ExceedQuota-ReplicaSet-refined-10.csv -o ./out-head-10/FailedCreate-ExceedQuota-ReplicaSet-refined-10-out-0628.json -b 0 -e 3
+
+# for FailedCreate-ExceedQuota-StatefulSet-refined-10
+python3 ../test_with_file_args_extend_index.py -i ./input-head-10/FailedCreate-ExceedQuota-StatefulSet-refined-10.csv -o ./out-head-10/FailedCreate-ExceedQuota-StatefulSet-refined-10-out-0628.json -b 0 -e 3
+
+# for FailedCreate-ServiceAccountNotFound-refined-10
+python3 ../test_with_file_args_extend_index.py -i ./input-head-10/FailedCreate-ServiceAccountNotFound-refined-10.csv -o ./out-head-10/FailedCreate-ServiceAccountNotFound-refined-10-out-0628.json -b 0 -e 3
+
