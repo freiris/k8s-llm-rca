04f98c8f526fdd766716addb3b4c4bedb015d601
ubuntu <xiangyong08@gmail.com>
2024-08-12 07:26:23 -0700

    run-100-xx in ubuntu-n166

--- full-test/run-100-Failed.sh
+++ full-test/run-100-Failed.sh
@@ -1,12 +1,24 @@
 # for Failed-AccessDenied-not
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-AccessDenied.csv -o ./output-4/Failed-AccessDenied-out-0722-azure.json -b 3 -e 100 
+# 38 examples, test split-run
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-AccessDenied.csv -o ./output-4/Failed-AccessDenied-out-0811-azure.json -b 0 -e 20 
+
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-AccessDenied.csv -o ./output-4/Failed-AccessDenied-out-0811-azure.json -b 20 -e 100 
+
 
 # for Failed-ArtifactNotFound
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-ArtifactNotFound-repeat.csv -o ./output-4/Failed-ArtifactNotFound-repeat-out-0722-azure.json -b 9 -e 100
+# 19 examples, previous result is good in a single run
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-ArtifactNotFound-repeat.csv -o ./output-4/Failed-ArtifactNotFound-repeat-out-0811-azure.json -b 0 -e 100
 
 # for Failed-NetworkUnreachable
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-NetworkUnreachable-repeat.csv -o ./output-4/Failed-NetworkUnreachable-repeat-out-0722-azure.json -b 3 -e 100
+# 20 examples, test split-run 
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-NetworkUnreachable-repeat.csv -o ./output-4/Failed-NetworkUnreachable-repeat-out-0811-azure.json -b 0 -e 10
+
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-NetworkUnreachable-repeat.csv -o ./output-4/Failed-NetworkUnreachable-repeat-out-0811-azure.json -b 10 -e 100
+
 
 # for Failed-NoVolumeToMount
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-NoVolumeToMount-repeat.csv -o ./output-4/Failed-NoVolumeToMount-repeat-out-0722-azure.json -b 3 -e 100
+# 23 examples, intrinsic difficult, test split-run
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-NoVolumeToMount-repeat.csv -o ./output-4/Failed-NoVolumeToMount-repeat-out-0811-azure.json -b 0 -e 10
+
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/Failed-NoVolumeToMount-repeat.csv -o ./output-4/Failed-NoVolumeToMount-repeat-out-0811-azure.json -b 10 -e 100
 
--- full-test/run-100-FailedCreate.sh
+++ full-test/run-100-FailedCreate.sh
@@ -1,15 +1,26 @@
 # for FailedCreate-ExceedQuota-Job
+# 45 examples, require split-run
+#python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-Job.csv -o ./output-4/FailedCreate-ExceedQuota-Job-out-0811-azure.json -b 0 -e 15
 
-#python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-Job.csv -o ./output-4/FailedCreate-ExceedQuota-Job-out-0722.json -b 0 -e 3
+#python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-Job.csv -o ./output-4/FailedCreate-ExceedQuota-Job-out-0811-azure.json -b 15 -e 30
 
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-Job.csv -o ./output-4/FailedCreate-ExceedQuota-Job-out-0810-debug-azure.json -b 1 -e 3
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-Job.csv -o ./output-4/FailedCreate-ExceedQuota-Job-out-0811-azure.json -b 30 -e 100
 
 # for FailedCreate-ExceedQuota-ReplicaSet
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-ReplicaSet.csv -o ./output-4/FailedCreate-ExceedQuota-ReplicaSet-out-0810-debug-azure.json -b 0 -e 3
+# 32 examples, require split-run
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-ReplicaSet.csv -o ./output-4/FailedCreate-ExceedQuota-ReplicaSet-out-0811-azure.json -b 0 -e 10
+
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-ReplicaSet.csv -o ./output-4/FailedCreate-ExceedQuota-ReplicaSet-out-0811-azure.json -b 10 -e 20
+
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-ReplicaSet.csv -o ./output-4/FailedCreate-ExceedQuota-ReplicaSet-out-0811-azure.json -b 20 -e 100
 
 # for FailedCreate-ExceedQuota-StatefulSet
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-StatefulSet-repeat.csv -o ./output-4/FailedCreate-ExceedQuota-StatefulSet-repeat-out-0810-debug-azure.json -b 0 -e 3 
+# 20 examples, split-run may help
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-StatefulSet-repeat.csv -o ./output-4/FailedCreate-ExceedQuota-StatefulSet-repeat-out-0811-azure.json -b 0 -e 10
+
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ExceedQuota-StatefulSet-repeat.csv -o ./output-4/FailedCreate-ExceedQuota-StatefulSet-repeat-out-0811-azure.json -b 10 -e 100
 
 # for FailedCreate-ServiceAccountNotFound
-python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ServiceAccountNotFound.csv -o ./output-4/FailedCreate-ServiceAccountNotFound-out-0810-debug-azure.json -b 0 -e 3
+# 32 examples, previous result is good in a single run
+python3 ../test_with_file_args_extend_index_azure.py -i ./data-4/FailedCreate-ServiceAccountNotFound.csv -o ./output-4/FailedCreate-ServiceAccountNotFound-out-0811-azure.json -b 0 -e 100
 
